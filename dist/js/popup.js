1f0c752b9f248293394011ea66ba1df06496aae9
nator333 <m.nakamata35@gmail.com>
2018-10-15 23:17:24 -0700

    Update popup
    When enter pressed, open new tab

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,9 +1,21 @@
 const lmdUtl = "https://www.ldoceonline.com/dictionary/";
 document.getElementById("lmd-search-btn").addEventListener("click", search);
+document.getElementById("lmd-text-input").addEventListener("keypress", enterSearch);
+
 function search() {
     chrome.tabs.create({
             active: true,
             url: lmdUtl + document.getElementById("lmd-text-input").value.replace(/[\.\*\?;!()\+,\[:\]<>^_`\[\]{}~\\\/\"\'=]/g, " ").trim()
         }
     );
+}
+
+function enterSearch(event){
+    if (event.defaultPrevented) {
+        return;
+    }
+
+    if (event.key === "Enter") {
+        search();
+    }
 }

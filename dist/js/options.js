752eb0cc26fcb79994a9291ba1abf0889f505c79
nator333 <m.nakamata35@gmail.com>
2018-10-15 23:18:56 -0700

    Fix not updated option

--- dist/js/options.js
+++ dist/js/options.js
@@ -1,29 +1,23 @@
-let isDisable = false;
-let showIconFirst = false;
-
 chrome.storage.local.get(['lmdIsDisable'], function (result) {
-    isDisable = result.lmdIsDisable;
+    document.getElementById("lmd-isDisable").checked = result.lmdIsDisable;
 });
 
 chrome.storage.local.get(['lmdShowIconFirst'], function (result) {
-    showIconFirst = result.lmdShowIconFirst;
+    let radios = document.getElementsByName("lmd-option");
+    if (result.lmdShowIconFirst) {
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked = radios[i].value === "1";
+        }
+    } else {
+        for (let i = 0; i < radios.length; i++) {
+            radios[i].checked = radios[i].value === "2";
+        }
+    }
 });
-
-
-document.getElementById("lmd-isDisable").checked = isDisable;
-let radios = document.getElementsByName("lmd-option");
-if (showIconFirst) {
-    for (let i = 0; i < radios.length; i++) {
-        radios[i].checked = radios[i].value === "1";
-    }
-} else {
-    for (let i = 0; i < radios.length; i++) {
-        radios[i].checked = radios[i].value === "2";
-    }
-}
 
 // AddEventListener
 document.getElementById("lmd-isDisable").addEventListener("change", checkboxChanged);
+let radios = document.getElementsByName("lmd-option");
 for (let i = 0; i < radios.length; i++) {
     radios[i].addEventListener("click", radioClicked);
 }
@@ -34,7 +28,9 @@ for (let i = 0; i < radios.length; i++) {
  * @param checkedElement
  */
 function checkboxChanged(checkedElement) {
-    chrome.storage.local.set({lmdIsDisable: checkedElement.target.checked}, () => {});
+    chrome.storage.local.set({lmdIsDisable: checkedElement.target.checked}, () => {
+    });
+    let radios = document.getElementsByName("lmd-option");
     for (let i = 0; i < radios.length; i++) {
         radios[i].disabled = checkedElement.target.checked
     }
@@ -47,10 +43,14 @@ function checkboxChanged(checkedElement) {
  */
 function radioClicked(clickedElement) {
     if (clickedElement.target.value === "1") {
-        chrome.storage.local.set({lmdIsDisable: false}, ()=>{});
-        chrome.storage.local.set({lmdShowIconFirst: true}, ()=>{});
+        chrome.storage.local.set({lmdIsDisable: false}, () => {
+        });
+        chrome.storage.local.set({lmdShowIconFirst: true}, () => {
+        });
     } else {
-        chrome.storage.local.set({lmdIsDisable: false}, ()=>{});
-        chrome.storage.local.set({lmdShowIconFirst: false}, ()=>{});
+        chrome.storage.local.set({lmdIsDisable: false}, () => {
+        });
+        chrome.storage.local.set({lmdShowIconFirst: false}, () => {
+        });
     }
 }
